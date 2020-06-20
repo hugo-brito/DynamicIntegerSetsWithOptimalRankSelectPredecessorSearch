1e29e7b582db183f6b2a0e0c39ecc0f74441e0d7
Hugo Brito <hubr@itu.dk>
2020-06-21 00:57:19 +0200

    Added JUnit testing framework. Works with VS Code.

---
+++ src/test/java/BinarySearchTrieTest.java
@@ -0,0 +1,66 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTrieTest {
+
+  BinarySearchTrie t;
+
+  @BeforeEach
+  void setUp() {
+    t = new BinarySearchTrie();
+  }
+
+  @AfterEach
+  void tearDown() {
+    t = null;
+  }
+
+  @Test
+  void size() {
+    assert (t == null);
+    assertFalse(2 == 2);
+  }
+
+  @Test
+  void insert() {
+  }
+
+  @Test
+  void delete() {
+  }
+
+  @Test
+  void member() {
+  }
+
+  @Test
+  void predecessor() {
+  }
+
+  @Test
+  void successor() {
+  }
+
+  @Test
+  void rank() {
+  }
+
+  @Test
+  void select() {
+  }
+
+  @Test
+  void count() {
+  }
+
+  @Test
+  void height() {
+  }
+
+  @Test
+  void countLeafNodes() {
+  }
+}
---
+++ src/test/java/BitsKeyTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BitsKeyTest {
+
+  BitsKey v;
+
+  @BeforeEach
+  void setUp() {
+
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void bit() {
+  }
+
+  @Test
+  void compareTo() {
+  }
+
+  @Test
+  void testEquals() {
+  }
+
+  @Test
+  void bin() {
+  }
+
+  @Test
+  void testToString() {
+  }
+}
