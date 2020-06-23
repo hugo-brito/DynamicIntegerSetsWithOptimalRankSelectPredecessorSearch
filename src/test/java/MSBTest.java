73431cc0b13728ba8505b7361568c97b9069a384
Hugo Brito <hubr@itu.dk>
2020-06-23 07:27:02 +0200

    Improved tests.

--- src/test/java/MSBTest.java
+++ src/test/java/MSBTest.java
@@ -4,57 +4,62 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MSBTest {
 
+  int intLowerBound = -100_000_000;
+  int intUpperBound = 100_000_000;
+  long longLowerBound = Integer.MIN_VALUE - 100_000_000;
+  long longUpperBound = Integer.MAX_VALUE + 100_000_000;
+
   @Test
   void msb32Obvious() {
-    for (int i = -1_000_000; i < 0; i++) {
+    for (int i = intLowerBound; i < 0; i++) {
       assertEquals(Integer.numberOfLeadingZeros(i), MSB.msb32Obvious(i));
     }
     assertEquals(-1, MSB.msb32Obvious(0));
-    for (int i = 1; i <= 1_000_000; i++) {
+    for (int i = 1; i <= intUpperBound; i++) {
       assertEquals(Integer.numberOfLeadingZeros(i), MSB.msb32Obvious(i));
     }
   }
 
   @Test
   void msb64Obvious() {
-    for (long i = -1_000_000; i < 0; i++) {
+    for (long i = longLowerBound; i < 0; i++) {
       assertEquals(Long.numberOfLeadingZeros(i), MSB.msb64Obvious(i));
     }
     assertEquals(-1, MSB.msb64Obvious(0));
-    for (long i = 1; i <= 1_000_000; i++) {
+    for (long i = 1; i <= longUpperBound; i++) {
       assertEquals(Long.numberOfLeadingZeros(i), MSB.msb64Obvious(i));
     }
   }
 
   @Test
   void msb32double() {
-    for (int i = -1_000_000; i < 0; i++) {
+    for (int i = intLowerBound; i < 0; i++) {
       assertEquals(Integer.numberOfLeadingZeros(i), MSB.msb32double(i));
     }
     assertEquals(-1, MSB.msb32double(0));
-    for (int i = 1; i <= 1_000_000; i++) {
+    for (int i = 1; i <= intUpperBound; i++) {
       assertEquals(Integer.numberOfLeadingZeros(i), MSB.msb32double(i));
     }
   }
 
   @Test
   void msb64LookupDistributedOutput() {
-    for (long i = -1_000_000; i < 0; i++) {
+    for (long i = longLowerBound; i < 0; i++) {
       assertEquals(Long.numberOfLeadingZeros(i), MSB.msb64LookupDistributedOutput(i));
     }
     // assertEquals(-1, MSB.msb64LookupDistributedOutput(0));
-    for (long i = 1; i <= 1_000_000; i++) {
+    for (long i = 1; i <= longUpperBound; i++) {
       assertEquals(Long.numberOfLeadingZeros(i), MSB.msb64LookupDistributedOutput(i));
     }
   }
 
   @Test
   void msb64LookupDistributedInput() {
-    for (long i = Long.MIN_VALUE; i < 0; i++) {
+    for (long i = longLowerBound; i < 0; i++) {
       assertEquals(Long.numberOfLeadingZeros(i), MSB.msb64LookupDistributedInput(i));
     }
     assertEquals(-1, MSB.msb64LookupDistributedInput(0));
-    for (long i = 1; i <= Long.MAX_VALUE - 1; i++) {
+    for (long i = 1; i <= longUpperBound; i++) {
       assertEquals(Long.numberOfLeadingZeros(i), MSB.msb64LookupDistributedInput(i));
     }
   }
