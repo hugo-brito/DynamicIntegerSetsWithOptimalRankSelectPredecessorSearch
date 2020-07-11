a75a8ad230fa8733a15309d5232489b2dd8fc18a
Hugo Brito <hubr@itu.dk>
2020-07-11 16:15:49 +0200

    Created class file to start implementing data structure from the paper.

---
+++ src/main/java/DynamicIntegerSetsWithOptimalRankSelectPredecessorSearch/protoPatrascuOptimal.java
@@ -0,0 +1,47 @@
+package DynamicIntegerSetsWithOptimalRankSelectPredecessorSearch;
+
+public class protoPatrascuOptimal implements RankSelectPredecessorUpdate {
+
+  static class helperClass { // this will potentially be a node
+
+    
+
+  }
+
+  @Override
+  public void insert(long x) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void delete(long x) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean member(long x) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public long rank(long x) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public long select(long rank) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public long size() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+}
