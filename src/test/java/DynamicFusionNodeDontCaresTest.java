61b65a0c4927b6af0d395a10b9d27f33bd39a501
Hugo Brito <hubr@itu.dk>
2020-08-09 14:22:52 +0200

    Added test class for the DynamicFusionNodeDontCares

---
+++ src/test/java/DynamicFusionNodeDontCaresTest.java
@@ -0,0 +1,86 @@
+import integersets.DynamicFusionNodeDontCares;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DynamicFusionNodeDontCaresTest {
+
+  static final long seed = 42;
+  static final int passes = 10_000;
+  static final int numKeys = 8;
+
+  private DynamicFusionNodeDontCares set;
+  private static RankSelectPredecessorUpdateTest test;
+
+  @BeforeAll
+  static void generateTests() {
+    test = new RankSelectPredecessorUpdateTest(seed, passes, numKeys);
+  }
+
+  @BeforeEach
+  void setUp() {
+    set = new DynamicFusionNodeDontCares();
+  }
+
+  @AfterEach
+  void tearDown() {
+    set = null;
+  }
+
+  @AfterAll
+  static void clear() {
+    test = null;
+  }
+
+  @Test
+  void insertAndMemberSmallTest() {
+    test.insertAndMemberSmallTest(set);
+  }
+
+  @Test
+  void smallCorrectnessTest() {
+    test.smallCorrectnessTest(set);
+  }
+
+  @Test
+  void insertThenMemberTest() {
+    test.insertThenMemberTest(set);
+  }
+
+  @Test
+  void insertThenDeleteRangeOfKeysTest() {
+    test.insertThenDeleteRangeOfKeysTest(set);
+  }
+
+  @Test
+  void insertThenDeleteRandomKeysTest() {
+    test.insertThenDeleteRandomKeysTest(set);
+  }
+
+  @Test
+  void deleteTest() {
+    test.deleteTest(set);
+  }
+
+  @Test
+  void sizeTest() {
+    test.sizeTest(set);
+  }
+
+  @Test
+  void growingRankTest() {
+    test.growingRankTest(set);
+  }
+
+  @Test
+  void selectOfRankTest() {
+    test.selectOfRankTest(set);
+  }
+
+  @Test
+  void rankOfSelectTest() {
+    test.rankOfSelectTest(set);
+  }
+}
